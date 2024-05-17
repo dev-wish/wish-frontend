3d0ecaec31b999b2cf745526dea1f110590bfa7b
Madfater <ericleeddd@gmail.com>
2024-05-18 00:41:07 +0800

    feat: 完成登入介面UI

---
+++ adventure_allies/src/app/MainPage/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1 className="justify-center align-middle text-center">Main Page</h1>
+      <h1 className="justify-center align-middle text-center">測試</h1>
+    </main>
+  );
+}
