77bfab6cbff0c53f699ffc5785d6e483d997f5fd
Madfater <ericleeddd@gmail.com>
2024-05-18 00:40:45 +0800

    feat: 新增字體

--- adventure_allies/tailwind.config.js
+++ adventure_allies/tailwind.config.js
@@ -1,15 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-
-    // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'login-page': "url('/public/images/mainPage.png')"
+      },
+      fontFamily: {
+        'chinese-font': ['chineseFont'],
+        'english-font': ['englishFont']
+      }
+    },
   },
   plugins: [],
 }
